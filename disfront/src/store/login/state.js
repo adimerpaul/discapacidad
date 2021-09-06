431542fe72ba124a7bc96cd7b4c9ce12fe654eab
adimerpaul <adimer101@gmail.com>
2021-09-06 10:13:50 -0400

    cambiado token

--- disfront/src/store/login/state.js
+++ disfront/src/store/login/state.js
@@ -1,7 +1,7 @@
 export default function () {
   return {
     status: '',
-    token: localStorage.getItem('token') || '',
+    token: localStorage.getItem('tokendisca') || '',
     user : {},
     pedido:[]
   }
